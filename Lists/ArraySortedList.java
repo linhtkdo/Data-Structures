f654b7166e3c24755a6ae3b780290946bd5659e8
Linh Do <linhdo@umass.edu>
2016-11-08 09:54:21 -0500

    Update ArraySortedList.java

--- Lists/ArraySortedList.java
+++ Lists/ArraySortedList.java
@@ -1,1 +1,17 @@
-
+public class ArraySortedList<T> extends ArrayUnsortedList<T> implements ListInterface<T> {
+  public void add (T e) {
+    T listElement;
+    int loc = 0;
+    if (numElements == list.length) enlarge();
+    while (loc < numElements) {
+      listElement = list[loc];
+      if (((Comparable<T>) listElement).compareTo(e) < 0)
+        loc++;
+      else break;
+    }
+    for (int index == numElements; index > loc; index--)
+      list[index] = list[index-1];
+    list[loc] = e;
+    numElements++;
+  }
+}
