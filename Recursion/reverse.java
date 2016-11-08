4f972b553a315b5cee3d979134bfa80bd887fc46
Linh Do <linhdo@umass.edu>
2016-11-08 11:03:41 -0500

    Update reverse.java

--- Recursion/reverse.java
+++ Recursion/reverse.java
@@ -1,1 +1,13 @@
-
+/* Reverse a Linked List */
+public void reverse(LLNode<T> curr) {
+  if (curr == null)
+    return;
+  if (curr.getLink() == null) {
+    head = curr;
+    return;
+  }
+  reverse(curr.getLink());
+  curr.getLink().setLink(curr);
+  curr.setLink(null);
+}
+  
