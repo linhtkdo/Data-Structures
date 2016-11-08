26bfc76c67c0697f2596f688e61fd33bcaa0a1ae
Linh Do <linhdo@umass.edu>
2016-11-08 11:25:08 -0500

    Update BSTNode.java

--- Trees/BSTNode.java
+++ Trees/BSTNode.java
@@ -1,1 +1,11 @@
-
+public class BSTNode<T extends Comparable<T>> {
+  protected T info;
+  protected BSTNode<T> left;
+  protected BSTNode<T> right;
+  
+  public BSTNode(T info) {
+    this.info = info;
+  }
+  
+  /* getters and setters for info, left, and right */
+}
