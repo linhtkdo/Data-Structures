70cbe1634e05d5661d7116cb5b6ecdc2395ef72f
Linh Do <linhdo@umass.edu>
2016-11-08 09:24:48 -0500

    Update IndexedListInterface.java

--- Lists/IndexedListInterface.java
+++ Lists/IndexedListInterface.java
@@ -1,1 +1,8 @@
-
+public interface IndexedListInterface<T> extends ListInterface<T> {
+  void add (int index, T e); // higher e's move up
+  T set (int index, T e); // returns former value
+  T get (int index); // exception for bad index
+  int indexOf (T e); // -1 if none
+  T remove (int index); // higher e's move down
+}
+  
