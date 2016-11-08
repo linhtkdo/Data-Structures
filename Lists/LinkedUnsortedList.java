fa24325ee3199e82b7f3e68e2d53f54e0edfb8a0
Linh Do <linhdo@umass.edu>
2016-11-08 10:15:26 -0500

    Update LinkedUnsortedList.java

--- Lists/LinkedUnsortedList.java
+++ Lists/LinkedUnsortedList.java
@@ -1,1 +1,35 @@
-
+public class LinkedUnsortedList<T> implements ListInterface<T> {
+  protected int numElements;
+  protected LLNode<T> list;
+  
+  private class findVal<T> {
+    public LLNode<T> curr, prev;
+    public findVal(LLNode<T> c, LLNode<T> p) {
+      curr = c; prev = p;
+    }
+  }
+  protected findVal<T> find (T target) {
+    LLNode<T> curr = list, prev = null;
+    while (curr != null) {
+      if (curr.getInfo().equals(target))
+        break;
+      else {
+        prev = curr;
+        curr = curr.getLink();
+      }
+    }
+    return new findVal(curr, prev);
+  }
+  public boolean remove(T e) {
+    findVal<T> fval = find(e);
+    if (fval.curr != null) {
+      if (fval.curr == list) 
+        list = list.getLink();
+      else 
+        fval.prev.setLink(fval.curr.getLink());
+      numElements--;
+    }
+    return (fval.curr != null); 
+  }
+}
+      
