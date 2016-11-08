655a6688c392ebc9010ae1bcea787de9b28bd21b
Linh Do <linhdo@umass.edu>
2016-11-08 10:59:03 -0500

    Update revPrint.java

--- Recursion/revPrint.java
+++ Recursion/revPrint.java
@@ -1,1 +1,11 @@
+/* Printing a List Backwards */
+public void revPrint() {
+  revPrint(head);
+}
 
+private void revPrint (LLNode<T> listRef) {
+  if (listRef != null) {
+    revPrint(listRef.getLink());
+    System.out.println(listRef.getInfo());
+  }
+}
