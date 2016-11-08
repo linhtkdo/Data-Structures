5060cf6febadc1f88bb162b5cd28ec9ce83d361c
Linh Do <linhdo@umass.edu>
2016-11-08 08:58:06 -0500

    Update LinkedUnbndQueue.java

--- Queues/LinkedUnbndQueue.java
+++ Queues/LinkedUnbndQueue.java
@@ -1,1 +1,24 @@
+public class LinkedUnbndQueue<T> implements UQI<T> {
+  protected LLNode<T> front;
+  protected LLNode<T> rear;
 
+  public void enqueue(T e) {
+    LLNode<T> nn = new LLNode<T>(e);
+    if (rear == null) 
+      front = nn;
+    else 
+      rear.setLink(nn);
+    rear = nn;
+  }
+  
+  public T dequeue() {
+    if (isEmpty()) 
+      throw new QueueUnderflowException("empty queue");
+    else {
+      T e = front.getInfo();
+      front = front.getLink();
+      if (front == null) rear = null;
+      return e;
+    }
+  }
+}
