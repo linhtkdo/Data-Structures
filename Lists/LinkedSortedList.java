c5392f7a5a1c239cda53ef43d6a605fed055d5c6
Linh Do <linhdo@umass.edu>
2016-11-08 10:33:17 -0500

    Update LinkedSortedList.java

--- Lists/LinkedSortedList.java
+++ Lists/LinkedSortedList.java
@@ -1,1 +1,26 @@
-
+public class LinkedSortedList <T extends Comparable<T>> extends LinkedUnsortedList<T> implements ListInterface<T> {
+  public LinkedSortedList() {
+    super();
+  }
+  public void add(T e) {
+    LLNode<T> curr = list, prev = null;
+    while (curr != null) { // pass all e's that are less than the new e
+      T curr_e = curr.getInfo();
+      if (curr_e.compareTo(e) < 0) {
+        prev = curr;
+        curr.getLink();
+      } else break;
+    }
+    LLNode<T> nn = new LLNode<T>(e);
+    if (prev == null) { // adding before head
+      nn.setLink(list);
+      list = nn;
+    } else {
+      nn.setLink(curr);
+      prev.setLink(nn);
+    }
+    numElements++;
+  }
+}
+      
+      
