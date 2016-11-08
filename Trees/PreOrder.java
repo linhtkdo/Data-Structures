e26a8731de5527a27aa87777fedf2d6969323364
Linh Do <linhdo@umass.edu>
2016-11-08 11:21:14 -0500

    Update PreOrder.java

--- Trees/PreOrder.java
+++ Trees/PreOrder.java
@@ -1,1 +1,7 @@
-public void preOrder(Tr
+public void preOrder(TreeNode x) {
+  if (x != null) {
+    System.out.println(x.getInfo());
+    preOrder(x.getLeft());
+    preOrder(x.getRight());
+  }
+}
