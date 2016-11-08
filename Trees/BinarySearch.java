ad915b452009938d54cbb08d5a7d9d8f996e0ec0
Linh Do <linhdo@umass.edu>
2016-11-08 11:17:26 -0500

    Update BinarySearch.java

--- Trees/BinarySearch.java
+++ Trees/BinarySearch.java
@@ -1,1 +1,26 @@
-protected int 
+protected int find (T target) {
+  int lower = 0, upper = numElements-1;
+  while (lower <= upper) {
+    int curr = (lower + upper) / 2;
+    int result = (Comparable)target.compareTo(list[curr]);
+    if (result == 0)
+      return curr;
+    else if (result < 0)
+      upper = curr - 1;
+    else lower = curr + 1;
+  }
+  return -1;
+}
+
+protected int recFind (Comparable target, int lower, int upper) {
+  if (lower > upper) return -1;
+  int curr = (lower + upper) / 2;
+  int result = target.compareTo(list[curr]);
+  if (result == 0) return curr;
+  else if (result < 0) return recFind (target, lower, curr - 1);
+  else return recFind (target, curr + 1, upper);
+}
+protected int find (T target) {
+  Comparable tar = (Comparable) target;
+  return recFind (tar, 0, numElements - 1);
+}
