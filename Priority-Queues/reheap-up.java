e62b285873ae7b020a44049f4ad8612a8d637d89
Linh Do <linhdo@umass.edu>
2016-11-25 13:26:57 -0500

    Update reheap-up.java

--- Priority-Queues/reheap-up.java
+++ Priority-Queues/reheap-up.java
@@ -1,1 +1,10 @@
-
+private void reheapUp(T elem) {
+  int hole = lastIndex;
+  int parent = (hole-1) / 2;
+  while ((hole > 0) && elem.compareTo(heap[parent]) > 0) {
+    heap[hole] = heap[parent];
+    hole = parent;
+    parent = (parent-1) / 2;
+  }
+  heap[hole] = elem;
+}
