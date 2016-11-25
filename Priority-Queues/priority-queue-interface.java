9cf1ea00ed262c67c4958231eb6aa0bf766bbc1b
Linh Do <linhdo@umass.edu>
2016-11-25 13:21:58 -0500

    Update priority-queue-interface.java

--- Priority-Queues/priority-queue-interface.java
+++ Priority-Queues/priority-queue-interface.java
@@ -1,1 +1,13 @@
-
+public interface PriorityQueueInterface<T extends Comparable<T>> {
+  boolean isEmpty();
+  boolean isFull();
+  T peek();
+  
+  void enqueue(T element);
+  // Throws PriorityQueueOverflowException if full;
+  // otherwise, adds element to this priority queue.
+  
+  T dequeue();
+  // Throws PriorityQueueUnderflowException if empty;
+  // otherwise, removes element with highest priority
+  // and returns it.
