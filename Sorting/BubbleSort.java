b9cc7057f9c3d6de05c6bb65e446a705d74e1f9f
Linh Do <linhdo@umass.edu>
2016-12-08 14:00:30 -0500

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,1 +1,14 @@
+public void bubbleSort() {
+  int out, in;
+  for (out=nElems-1; out>0; out--)  // outer loop
+    for (in=0; in<out; in++)        // inner loop
+      if (a[in].compareTo(a[in+1]) > 0)
+        swap(in, in+1);
+}
 
+public void swap(int i, int j) {
+  T temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
+  
