cacc7a0ebc23806c1fefd98a65369018901e28d2
Linh Do <linhdo@umass.edu>
2016-12-08 14:06:11 -0500

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,1 +1,10 @@
-
+public void selectionSort() {
+  int out, in, min;
+  for (out=0; out<nElems-1; out++) { // outer loop
+    min = out;
+    for (in=out+1; in<nElems; in++)  // inner loop
+      if (a[in].compareTo(a[min]) < 0) min = in;
+    swap(out, min);
+  }
+}
+      
