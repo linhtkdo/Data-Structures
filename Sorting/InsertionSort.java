51ac167a97296a5ebd33b0eec0ee386a77e05e64
Linh Do <linhdo@umass.edu>
2016-12-08 14:37:26 -0500

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,1 +1,12 @@
-public void insertionSort() 
+public void insertionSort() {
+  int out, in;
+  for (out=1; out<nElems; out++) { // dividing line
+    T temp = a[out];
+    in = out; // start shifts at out
+    while (in>0 && a[in-1].compareTo(temp)>0) {
+      a[in] = a[in-1];  // shift right until the 
+      in--;             // element that's no longer larger
+    }
+    a[in] = temp; // copy
+  }
+}
